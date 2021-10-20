be35ca53510ccd5100816496cc0f9904e2312a34
DarianAntonio <92097147+DarianAntonio@users.noreply.github.com>
2021-10-20 22:20:55 +0100

    Add files via upload

---
+++ Chess Game/main.cpp
@@ -0,0 +1,203 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_ttf.h>
+#include <string>
+#include <stdio.h>
+
+
+SDL_Window* gWindow=NULL;
+
+SDL_Renderer* gRenderer = NULL;
+
+int screenWidth=600;
+int screenHeight=600;
+
+#include "texture.h"
+
+enum pieceName
+{
+    whitePawn=0,
+    whiteBishop=1,
+    whiteKnight=2,
+    whiteRock=3,
+    whiteQueen=4,
+    whiteKing=5,
+    blackPawn=6,
+    blackBishop=7,
+    blackKnight=8,
+    blackRock=9,
+    blackQueen=10,
+    blackKing=11
+};
+int board[8][8]={blackRock,blackKnight,blackBishop,blackQueen,blackKing,blackBishop,blackKnight,blackRock,
+                 blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,blackPawn,
+                 -1,-1,-1,-1,-1,-1,-1,-1,
+                 -1,-1,-1,-1,-1,-1,-1,-1,
+                 -1,-1,-1,-1,-1,-1,-1,-1,
+                 -1,-1,-1,-1,-1,-1,-1,-1,
+                 whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,whitePawn,
+                 whiteRock,whiteKnight,whiteBishop,whiteQueen,whiteKing,whiteBishop,whiteKnight,whiteRock};
+
+texture blackSquare,whiteSquare;
+texture chessPiece[12];
+
+bool Init_SDL()
+{
+    //Initialization flag
+	bool success = true;
+
+	//Initialize SDL
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
+	{
+		printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
+		success = false;
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
+		{
+			printf( "Warning: Linear texture filtering not enabled!" );
+		}
+
+		//Create window
+		gWindow = SDL_CreateWindow( "Chess Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN );
+		if( gWindow == NULL )
+		{
+			printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+			success = false;
+		}
+		else
+		{
+		    SDL_SetWindowResizable(gWindow,SDL_TRUE);
+			//Create renderer for window
+			gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+			if( gRenderer == NULL )
+			{
+				printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+				success = false;
+			}
+			else
+			{
+				//Initialize renderer color
+				SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+				//Initialize PNG loading
+				int imgFlags = IMG_INIT_PNG;
+				if( !( IMG_Init( imgFlags ) & imgFlags ) )
+				{
+					printf( "SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError() );
+					success = false;
+				}
+			}
+		}
+	}
+
+	return success;
+}
+
+bool Load_Media()
+{
+    blackSquare.Load_Image(gRenderer,"blackSquare.png");
+    whiteSquare.Load_Image(gRenderer,"whiteSquare.png");
+    chessPiece[whitePawn].Load_Image(gRenderer,"wP.png");
+    chessPiece[whiteBishop].Load_Image(gRenderer,"wB.png");
+    chessPiece[whiteKnight].Load_Image(gRenderer,"wN.png");
+    chessPiece[whiteRock].Load_Image(gRenderer,"wR.png");
+    chessPiece[whiteQueen].Load_Image(gRenderer,"wQ.png");
+    chessPiece[whiteKing].Load_Image(gRenderer,"wK.png");
+    chessPiece[blackPawn].Load_Image(gRenderer,"bP.png");
+    chessPiece[blackBishop].Load_Image(gRenderer,"bB.png");
+    chessPiece[blackKnight].Load_Image(gRenderer,"bN.png");
+    chessPiece[blackRock].Load_Image(gRenderer,"bR.png");
+    chessPiece[blackQueen].Load_Image(gRenderer,"bQ.png");
+    chessPiece[blackKing].Load_Image(gRenderer,"bK.png");
+}
+
+void Close_SDL()
+{
+    chessPiece[whitePawn].Free_Texture();
+    chessPiece[whiteBishop].Free_Texture();
+    chessPiece[whiteKnight].Free_Texture();
+    chessPiece[whiteRock].Free_Texture();
+    chessPiece[whiteQueen].Free_Texture();
+    chessPiece[whiteKing].Free_Texture();
+    chessPiece[blackPawn].Free_Texture();
+    chessPiece[blackBishop].Free_Texture();
+    chessPiece[blackKnight].Free_Texture();
+    chessPiece[blackRock].Free_Texture();
+    chessPiece[blackQueen].Free_Texture();
+    chessPiece[blackKing].Free_Texture();
+    blackSquare.Free_Texture();
+    whiteSquare.Free_Texture();
+
+    SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
+
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}
+
+int main(int argc,char* args[])
+{
+    Init_SDL();
+    Load_Media();
+    bool quit = false;
+
+    //Event handler
+    SDL_Event e;
+    int prevHeight=0,prevWidth=0;
+    //While application is running
+    while( !quit )
+    {
+        //Handle events on queue
+        while( SDL_PollEvent( &e ) != 0 )
+        {
+            //User requests quit
+            if( e.type == SDL_QUIT )
+            {
+                quit = true;
+            }
+        }
+
+        int side=screenHeight;
+        if(side>screenWidth)
+                    side=screenWidth;
+        side=side-side%8;
+        if(prevHeight!=screenHeight||prevWidth!=screenWidth)
+            SDL_SetWindowSize(gWindow,side,side);
+        prevHeight=screenHeight;
+        prevWidth=screenWidth;
+        SDL_GetWindowSize(gWindow,&screenWidth,&screenHeight);
+        //Clear screen
+        SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+        SDL_RenderClear( gRenderer );
+        //Render background texture to screen
+
+        for(int i=0;i<8;i++)
+        {
+
+         int sumI=side/8;
+         sumI=sumI*i;
+            for(int j=0;j<8;j++)
+            {
+                int sumJ=side/8;
+                sumJ=sumJ*j;
+
+                if(i%2==j%2)
+                    whiteSquare.Render_Texture(gRenderer,sumJ,sumI,0.125);
+                else
+                    blackSquare.Render_Texture(gRenderer,sumJ,sumI,0.125);
+                if(board[i][j]!=-1)
+                    chessPiece[board[i][j]].Render_Texture(gRenderer,sumJ,sumI,0.125);
+            }
+        }
+        //Update screen
+        SDL_RenderPresent( gRenderer );
+    }
+    Close_SDL();
+    return 0;
+}
---
+++ Chess Game/texture.h
@@ -0,0 +1,98 @@
+#ifndef TEXTURE_H_INCLUDED
+#define TEXTURE_H_INCLUDED
+
+class texture
+{
+    public:
+        //Initiates variables
+        texture();
+
+        //Deallocator
+        ~texture();
+
+        //Loads texture from image file
+        bool Load_Image( SDL_Renderer* aRenderer , std::string aPath);
+
+        //Loads texture from text
+        bool Load_Text(std::string lText);
+
+        //Erase existing texture
+        void Free_Texture();
+
+        void setColor( Uint8 red, Uint8 green, Uint8 blue );
+
+        void Render_Texture(SDL_Renderer* aRenderer , int x , int y , float size , SDL_Rect* clip);
+
+    private:
+        //Image texture
+        SDL_Texture* mTexture;
+
+        //Image dimensions
+        int mWidth;
+        int mHeight;
+
+};
+
+texture::texture()
+{
+    mTexture=NULL;
+    mWidth=0;
+    mHeight=0;
+}
+
+texture::~texture()
+{
+   Free_Texture();
+}
+
+void texture::Free_Texture()
+{
+    if(mTexture!=NULL)
+    {
+        mTexture=NULL;
+        mWidth=0;
+        mHeight=0;
+    }
+}
+
+bool texture::Load_Image( SDL_Renderer* aRenderer , std::string aPath )
+{
+    //Get rid of preexisting texture
+	Free_Texture();
+
+	//The final texture
+	SDL_Texture* lTexture = NULL;
+
+	//Load image at specified path
+	SDL_Surface* lSurface = IMG_Load( aPath.c_str() );
+
+	lTexture=SDL_CreateTextureFromSurface(aRenderer,lSurface);
+
+
+	mTexture=lTexture;
+	mHeight=lSurface->h;
+	mWidth=lSurface->w;
+	SDL_FreeSurface(lSurface);
+	return mTexture != NULL;
+}
+
+void texture::Render_Texture(SDL_Renderer* aRenderer , int x , int y , float size , SDL_Rect* clip=NULL)
+{
+    int side=screenHeight;
+    if(side>screenWidth)
+        side=screenWidth;
+    float ratio=1;
+    int w,h;
+    w=side*size;
+    h=(side*size)/ratio;
+    SDL_Rect renderQuad={x,y,w,h};
+    SDL_RenderCopy(aRenderer,mTexture,clip,&renderQuad);
+}
+
+void texture::setColor( Uint8 red, Uint8 green, Uint8 blue )
+{
+	//Modulate texture rgb
+	SDL_SetTextureColorMod( mTexture, red, green, blue );
+}
+
+#endif // TEXTURE_H_INCLUDED
